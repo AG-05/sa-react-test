5a362859de6d39fc700a66c6903439f0fc246834
a.gurevich <a.gurevich@leboutique.com>
2020-03-05 19:06:32 +0200

    small refactoring of population model

--- src/models/population/index.js
+++ src/models/population/index.js
@@ -19,36 +19,47 @@ class Population {
         } 
     }
 
-    tick() {
+    tick(): void {
         let newPopulation: Array<Array<number>> = [];
-        const neighborsIndexs: Array<number> = [-1, 0, 1];
 
-        for (let i: number = 0; i < this.height; i++) 
-        {
+        this.currentPoppulation.map((rows, i) => {
             newPopulation[i] = [];
-            for (let j: number = 0; j < this.width; j++) 
-            {
-                let aliveNeighbours: number = 0;
-                neighborsIndexs.map(x => {
-                    neighborsIndexs.map(y => {
-                        aliveNeighbours += this.currentPoppulation[i + x] && this.currentPoppulation[i + x][j + y] ? this.currentPoppulation[i + x][j + y] : 0;
+            rows.map((cell, j) => {
+                const aliveNeighbours = this.getAliveNeighbors(i, j);
+                newPopulation[i][j] = this.getNextPopulation(cell, aliveNeighbours);
+                return null;
+            })
+            return null;
+        })
 
-                        return null;
-                    });
-
-                    return null;
-                });
-                aliveNeighbours -= this.currentPoppulation[i][j];
-                // Any live cell with fewer than two live neighbours (underpopulation) and more than three live neighbours (overcrowding) dies.
-                // Any live cell with two or three live neighbours lives on to the next generation.
-                if ((this.currentPoppulation[i][j] === 1)) { 
-                    newPopulation[i][j] = aliveNeighbours === 3 || aliveNeighbours === 2 ? 1 : 0; 
-                } else { // Any dead cell with exactly three live neighbours becomes a live cell (reproduction).
-                    newPopulation[i][j] = aliveNeighbours === 3 ? 1 : 0; 
-                }
-            } 
-        } 
         this.currentPoppulation = newPopulation;
+    }
+
+    getAliveNeighbors(i: number, j: number): number {
+        const neighborsIndexs: Array<number> = [-1, 0, 1];
+        let aliveNeighbours: number = 0;
+
+        neighborsIndexs.map(x => {
+            neighborsIndexs.map(y => {
+                aliveNeighbours += this.currentPoppulation[i + x] && this.currentPoppulation[i + x][j + y] ? this.currentPoppulation[i + x][j + y] : 0;
+                return null;
+            });
+            return null;
+        });
+        aliveNeighbours -= this.currentPoppulation[i][j];
+
+        return aliveNeighbours;
+    }
+
+    getNextPopulation(currentCell: number, aliveNeighbours: number): number {
+        // Any live cell with fewer than two live neighbours (underpopulation) and more than three live neighbours (overcrowding) dies.
+        // Any live cell with two or three live neighbours lives on to the next generation.
+        if (currentCell === 1) { 
+            return aliveNeighbours === 3 || aliveNeighbours === 2 ? 1 : 0; 
+        }
+
+        // Any dead cell with exactly three live neighbours becomes a live cell (reproduction).
+        return aliveNeighbours === 3 ? 1 : 0; 
     }
 }
 
